f8685617f94454ded34457f7b28c7d55e389604c
ajensen1234 <andrewjensen321@gmail.com>
2023-12-03 18:55:16 -0500

    Returning day03 input as 0,1,2 (as intended)

--- src/day03/data_loading.rs
+++ src/day03/data_loading.rs
@@ -6,12 +6,27 @@ pub struct Day03Data {
 impl Day03Data {
     pub fn new(data_path: &str) -> Result<Self, std::io::Error> {
         let input = fs::read_to_string(data_path)?;
-        // match fs::read_to_string(data_path) {
-        //     Ok(c) => println!("Yay: {:?}", c),
-        //     Err(c) => println!("Err: {:?}", c),
-        // }
         for line in input.lines() {
             println!("Line: {:?}", line);
+        }
+        let binary_input: Vec<Vec<i32>> = input
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|e| {
+                        if e.is_numeric() {
+                            1
+                        } else if (e == '.') {
+                            0
+                        } else {
+                            2
+                        }
+                    })
+                    .collect()
+            })
+            .collect();
+        for val in binary_input {
+            println!("Val: {:?}", val);
         }
         let din: Vec<i32> = Vec::new();
         Ok(Self { input: din })
